839b1d6859822fadf4b56162eb86826a9089f276
ben lyons <jesterscup@gmail.com>
2011-07-27 12:32:53 +0100

    added all available icon options to form

--- isotype_form.php
+++ isotype_form.php
@@ -7,7 +7,17 @@ title: <input type="text" name="title">
 <br />
 icon type:  
 	<select name="type">
-		<option name="man">man</option>
+		<option name="man">       man       </option>
+		<option name="woman">     woman     </option>
+		<option name="baby">      baby      </option>
+		<option name="house">     house     </option>
+		<option name="euro">      euro      </option>
+		<option name="ambulance"> ambulance </option>
+		<option name="bed">       bed       </option>
+		<option name="old">       old       </option>
+		<option name="hospital">  hospital  </option>
+		<option name="beer">      beer      </option>
+		<option name="bus">       bus       </option>
 	</select>
 <br />
 value:< input type="text" name="value">
